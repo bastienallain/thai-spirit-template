60cb6ddd6330f46f1ab1e1a1a15be78980c5dab1
Vivacoda <75243836+vivacoda@users.noreply.github.com>
2023-09-02 22:17:25 +0700

    Update .tsx

--- app/blog/[...slug]/.tsx
+++ app/blog/[...slug]/.tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import Head from "next/head";
 import { format, parseISO } from "date-fns";
 import { allPosts } from "contentlayer/generated";
 import { getMDXComponent } from "next-contentlayer/hooks";
@@ -7,13 +8,17 @@ import Image from "next/image";
 interface ParamsType {
   slug: string[];
 }
+
+interface Metadata {
+  title: string;
+  description: string;
+}
+
 interface Props {
   params?: ParamsType;
-  metadata?: {
-    title: string;
-    description: string;
-  };
+  metadata?: Metadata | null;
 }
+
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = allPosts.map((post) => ({
     params: { slug: post._raw.flattenedPath.split("/") },
@@ -22,21 +27,26 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
-  if (!params || !Array.isArray(params.slug)) {
+  try {
+    if (!params || !Array.isArray(params.slug)) {
+      return { props: {} };
+    }
+    const slugAsString = params.slug.join("/");
+    const post = allPosts.find(
+      (post) => post._raw.flattenedPath === slugAsString
+    );
+    return {
+      props: {
+        params: { slug: slugAsString },
+        metadata: post
+          ? { title: post.title, description: post.metaDescription }
+          : null,
+      },
+    };
+  } catch (error) {
+    // handle your error appropriately here
     return { props: {} };
   }
-  const slugAsString = params.slug.join("/");
-  const post = allPosts.find(
-    (post) => post._raw.flattenedPath === slugAsString
-  );
-  return {
-    props: {
-      params: { slug: slugAsString },
-      metadata: post
-        ? { title: post.title, description: post.metaDescription }
-        : null,
-    },
-  };
 };
 
 const PostLayout: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({
@@ -57,23 +67,33 @@ const PostLayout: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({
   }
 
   const Content = getMDXComponent(post.body.code);
+
   return (
-    <article className="mx-auto">
-      <div className="mb-8 text-center">
-        <Image
-          alt={post.title}
-          className="z-0 w-full h-[45vh] object-contain"
-          src={post.coverImage || "/test.jpeg"}
-          height={1200}
-          width={900}
+    <>
+      <Head>
+        <title>{metadata?.title || "Default Title"}</title>
+        <meta
+          name="description"
+          content={metadata?.description || "Default Description"}
         />
-        <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
-          {format(parseISO(post.date), "LLLL d, yyyy")}
-        </time>
-        <h1>{post.title}</h1>
-      </div>
-      <Content />
-    </article>
+      </Head>
+      <article className="mx-auto">
+        <div className="mb-8 text-center">
+          <Image
+            alt={post.title}
+            className="z-0 w-full h-[45vh] object-contain"
+            src={post.coverImage || "/test.jpeg"}
+            height={1200}
+            width={900}
+          />
+          <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
+            {format(parseISO(post.date), "LLLL d, yyyy")}
+          </time>
+          <h1>{post.title}</h1>
+        </div>
+        <Content />
+      </article>
+    </>
   );
 };
 
